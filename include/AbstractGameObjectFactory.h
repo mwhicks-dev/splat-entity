b0d9d6ee2d001a9dcd1844eaf032d2dad6c4d490
Mason <mason@hicksm.dev>
2024-10-03 12:29:58 -0400

    Added comments to GameObject and creational patterns #2 #4

--- include/AbstractGameObjectFactory.h
+++ include/AbstractGameObjectFactory.h
@@ -4,12 +4,31 @@ namespace SPlat {
 
     namespace Entity {
 
+        /// @brief Abstract factory class to create GameObjects
+        ///
+        /// Abstract wrapper for GameObject creation. Even though GameObject
+        /// is not subclassable, each concrete GameObjectFactory has its own 
+        /// builder used to add varying combinations of components and assets.
         class AbstractGameObjectFactory {
 
+        protected:
+
+            /// @brief builder used to construct GameObjects
+            ///
+            /// @see GameObjectBuilder
             GameObjectBuilder builder;
 
         public:
 
+            /// @brief function that builds a new GameObject prototype
+            /// @param id the ID of the GameObject to create
+            /// @return newly constructed GameObject
+            ///
+            /// Uses the GameObjectFactory's builder to initialize a new 
+            /// GameObject with the passed ID and then add a preselected 
+            /// combination of asset and component objects to it
+            ///
+            /// @see GameObject
             virtual GameObject* manufacture(size_t) = 0;
 
         };
--- include/GameObjectBuilder.h
+++ include/GameObjectBuilder.h
@@ -4,12 +4,27 @@ namespace SPlat {
 
     namespace Entity {
 
+        /// @brief builder class for GameObject instances
+        ///
+        /// This Builder pattern is used by AbstractGameObjetFactory 
+        /// implementations in order to simplify the creational process down 
+        /// to exclusively what is different about the game objects.
+        ///
+        /// @see AbstractGameObjectFactory
         class GameObjectBuilder {
 
+            /// @brief pointer to current GameObject instance
+            ///
+            /// This pointer contains the life cycle of GameObject creation.
+            /// The GameObjectBuilder is designed to run safely if a GameObject
+            /// is "built" without being created, but if this happens, a null 
+            /// pointer will be returned.
             GameObject * current = nullptr;
 
         public:
 
+            /// @brief 
+            /// @param id the ID of the new GameObject
             void initialize(const size_t);
 
             void add_component(Component&);
